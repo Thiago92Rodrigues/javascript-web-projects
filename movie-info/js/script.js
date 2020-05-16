d4c084444e1f65f72f92fe99c5971bb1ff1fe530
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-05-16 14:16:03 -0300

    place script.js in js folder

--- movie-info/js/script.js
+++ movie-info/js/script.js
@@ -1,21 +1,22 @@
 $(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
-        let searchText = $('#searchText').val();
-        getMovies(searchText);
-        e.preventDefault();
-    });
+  $("#searchForm").on("submit", (e) => {
+    let searchText = $("#searchText").val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
 });
 
 function getMovies(searchText) {
-    axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=thewdb')
+  axios
+    .get("http://www.omdbapi.com?s=" + searchText + "&apikey=thewdb")
     .then((response) => {
-        console.log(response);
-        let movies = response.data.Search;
-        let output = '';
+      console.log(response);
+      let movies = response.data.Search;
+      let output = "";
 
-        $.each(movies, (index, movie) => {
-            output += `
-                <div class="col-md-3">
+      $.each(movies, (index, movie) => {
+        output += `
+                <div class="col-md-2">
                     <div class="well text-center">
                         <img src="${movie.Poster}">
                         <h5>${movie.Title}</h5>
@@ -23,29 +24,30 @@ function getMovies(searchText) {
                     </div>
                 </div>
             `;
-        });
+      });
 
-        $('#movies').html(output);
+      $("#movies").html(output);
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
 }
 
 function movieSelected(id) {
-    sessionStorage.setItem('movieId', id);
-    window.location = 'movie.html';
-    return false;
+  sessionStorage.setItem("movieId", id);
+  window.location = "movie.html";
+  return false;
 }
 
 function getMovie() {
-    let movieId = sessionStorage.getItem('movieId');
+  let movieId = sessionStorage.getItem("movieId");
 
-    axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
+  axios
+    .get("http://www.omdbapi.com?i=" + movieId + "&apikey=thewdb")
     .then((response) => {
-        console.log(response);
-        let movie = response.data;
-        let output = `
+      console.log(response);
+      let movie = response.data;
+      let output = `
             <div class="row">
                 <div class="col-md-4">
                     <img src="${movie.Poster}" class="thumbnail">
@@ -75,9 +77,9 @@ function getMovie() {
             </div>
         `;
 
-        $('#movie').html(output);
+      $("#movie").html(output);
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
 }
